89c6a13b5c6e87e32a678e3142cecbba12e1643a
edlangjr <edlangjr@hotmail.com>
2018-01-03 16:50:34 -0500

    Initial UI Wireframes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { OrderBookComponent } from './order-book/order-book.component';
+import { SellOrdersComponent } from './sell-orders/sell-orders.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent,
+    OrderBookComponent,
+    SellOrdersComponent,
+    OrderHistoryComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
