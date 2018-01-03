a0d616958070a556954706d5eb7397d5d341a285
edlangjr <edlangjr@hotmail.com>
2018-01-03 17:01:19 -0500

    merge conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/app.component';
 import { HeaderComponent } from './components/header/header.component';
-<<<<<<< HEAD
 import { MenuComponent } from './menu/menu.component';
 import { OrderBookComponent } from './order-book/order-book.component';
 import { SellOrdersComponent } from './sell-orders/sell-orders.component';
 import { OrderHistoryComponent } from './order-history/order-history.component';
 import { FooterComponent } from './footer/footer.component';
-=======
 import { TestComponentComponent } from './components/test-component/test-component.component';
->>>>>>> d59defdf4a2ee2046d65bb600b7989c34dca449c
 
 import { ApiService } from './services/api.service';
 import { EthService } from './services/eth.service';
@@ -22,15 +19,12 @@ import { EthService } from './services/eth.service';
   declarations: [
     AppComponent,
     HeaderComponent,
-<<<<<<< HEAD
     MenuComponent,
     OrderBookComponent,
     SellOrdersComponent,
     OrderHistoryComponent,
-    FooterComponent
-=======
+    FooterComponent,
     TestComponentComponent
->>>>>>> d59defdf4a2ee2046d65bb600b7989c34dca449c
   ],
   imports: [
 		BrowserModule,
